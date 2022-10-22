28b9bbc400a0be38105b7c35d1bc4bae890e0463
huku3 <mhideyuki7777@icloud.com>
2022-10-22 15:49:10 +0900

    照会画面のボタン

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -75,7 +75,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('posts.show', compact('post'));
     }
 
     /**
