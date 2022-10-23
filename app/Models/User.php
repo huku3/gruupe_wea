48bef96a27a79d038581d388ad1e39ae5b62e018
huku3 <mhideyuki7777@icloud.com>
2022-10-23 09:31:12 +0900

    コメントモデル作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,11 @@ class User extends Authenticatable
 
     public function posts()
     {
-        return $this->hasMany(post::class);
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
